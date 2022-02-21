581ae6e69278178290e36bcba6552dfd83f74990
muh0108 <fhdnem0728@gmail.com>
2022-02-21 11:44:27 +0900

    delete

--- eGovframe/member_project/src/main/java/egovframework/example/sample/service/MemberService.java
+++
@@ -1,8 +0,0 @@
-package egovframework.example.sample.service;
-
-public interface MemberService {
-	/**등록*/
-	public String insertMember(MemberVO vo) throws Exception;
-	public MemberVO loginYn(String mem_uid) throws Exception;
-	
-}
